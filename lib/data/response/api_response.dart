d24403c30d128e0051697d77df88c6ef5904daaa
Mehedi Hasan <mehedihasanmax95@gmail.com>
2022-08-18 20:56:30 +0600

    handle app APIs response

---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_mvvm/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status, this.data, this.message);
+
+  ApiResponse.loading() : status = Status.loading;
+  ApiResponse.complete() : status = Status.complete;
+  ApiResponse.error() : status = Status.error;
+
+  @override
+  String toString() {
+    return "Status : $status \n Message : $message \n Data : $data";
+  }
+}
