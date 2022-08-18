b7b3fb0034c3c30bde857c3b0c88b7954d049910
Mehedi Hasan <mehedihasanmax95@gmail.com>
2022-08-18 20:19:40 +0600

    app exception handler added

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,26 @@
+class AppException implements Exception {
+  final String? _message;
+  final String? _prefix;
+  AppException([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return '$_prefix $_message';
+  }
+}
+
+class FetchDataException extends AppException {
+  FetchDataException([String? message]):super(message,"Error during communication");
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]):super(message,"Invalid Request");
+}
+
+class UnauthorizedException extends AppException {
+  UnauthorizedException([String? message]):super(message,"UnAuthorized Request");
+}
+
+class InvalidException extends AppException {
+  InvalidException([String? message]):super(message,"Invalid Input");
+}
